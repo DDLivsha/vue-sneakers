7958ca1e3554e9df1ac980fc40200cdb7d55dc64
Livsha <ddlivsha@gmail.com>
2025-08-17 14:04:17 +0300

    cart item

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import CardList from './components/CardList.vue';
+import Drawer from './components/Drawer.vue';
 import MyHeader from './components/MyHeader.vue';
 </script>
 
 <template>
-   <div class="w-4/5 mx-auto min-h-screen bg-slate-100 rounded-xl shadow-xl mt-14">
+   <Drawer />
+   <div class="w-4/5 mx-auto bg-slate-100 rounded-xl shadow-xl mt-14">
       <MyHeader />
       <div class="p-10">
          <h2 class="text-3xl font-bold">Усі кросівки</h2>
--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
+import CartItemList from './CartItemList.vue';
+import DrawerHead from './DrawerHead.vue';
 
 </script>
 
 <template>
-   <div class="fixed top-0 left-0 ">
-
+   <!-- overlay -->
+   <div class="fixed top-0 left-0 h-full w-full bg-black opacity-60 z-10 "></div>
+   <!-- drawer -->
+   <div class="bg-white w-96 h-full fixed right-0 top-0 z-20 p-8">
+      <DrawerHead />
+      <CartItemList/>
    </div>
 </template>
